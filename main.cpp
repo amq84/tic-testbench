d2f3b1b4defce0e2096bebaa77fbaa2cf2885bfb
amq <abelmqueralto@gmail.com>
2024-05-15 22:03:56 +0200

    Add current sensing

--- main.cpp
+++ main.cpp
@@ -6,12 +6,32 @@
 #include "mbed.h"
 #include "Tic.hpp"
 #include <cstdio>
+#include <chrono>
+#include <ratio>
 
-
+AnalogIn current_mV(A0);
+Timeout t;
 TicT500 tic;
+
+
+MemoryPool<float, 1024> mpool;
+Queue<float, 1024> queue;
+
+void currentRead()
+{
+    
+}
+
+void currentPrint()
+{
+
+}
+
 int main()
 {
 printf("Start testbench \n\r");
+t.attach_us(&currentRead, 1000);
+t.attach(&currentPrint, 1);
 
     while (true) {
 
